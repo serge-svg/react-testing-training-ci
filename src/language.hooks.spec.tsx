3faf4efb3d0bde2ed4198fa46fc350b907894609
galdons <sergivaque@gmail.com>
2020-10-21 14:58:16 +0200

    Initial commit

---
+++ src/language.hooks.spec.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { LanguageProvider } from './language.context';
+import { useLanguage } from './language.hooks';
+
+describe('useLanguage specs', () => {
+  it('should return a message with language equals "es" when it renders the hook', () => {
+    // Arrange
+    // const provider: React.FunctionComponent = (props) => {
+    //   return <LanguageProvider>{props.children}</LanguageProvider>;
+    // };
+
+    // Act
+    const { result } = renderHook(() => useLanguage(), {
+      wrapper: LanguageProvider,
+    });
+
+    act(() => {
+      result.current.setLanguage('es');
+    });
+
+    // Assert
+    expect(result.current.message).toEqual('The current language is: es');
+  });
+});
